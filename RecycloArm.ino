06e8fca9f0513b1172d5364170fc285c0d71bc87
José Bernal <80286949+darkMatter005@users.noreply.github.com>
2021-03-31 10:52:32 -0700

    Update RecycloArm.ino

--- RecycloArm.ino
+++ RecycloArm.ino
@@ -5,7 +5,6 @@
 
 #include <Braccio.h>
 #include <Servo.h>
-#include <math.h>
 #include <NewPing.h>
 
 
@@ -45,7 +44,7 @@ void grabbing(){
   //When in this position the end effector is approx 448.35 mm away from the base.
 }
 
-void rotation(){
+void loop(){
   //Given X, and Y from the camera, we calculate the angle needed for our arm
   //to rotate to allow our arm to grab the can. 
   float X, Y, T_rotation, Rotation, pi;//These values are given to us from our camera
@@ -77,10 +76,30 @@ void rotation(){
   //Begin rotation after doing the math.
   Braccio.ServoMovement(20,         Rotation, 45, 180, 180, 90, 73);//Doesnt work, think 
   //Rotation variable is messing it up.
-}
+  
+  //For this function we need the 3 angles (M2, M3, M4) with regards to the ultrasonic sensor
+  //information.
+  //To have the sensor trigger we use the following:
+  digitalWrite(trig, LOW);
+  delayMicroseconds(100);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(100);
+  digitalWrite(trig, LOW);
+  //Next we need ti define the distance it will be using.
+  float distance = pulseIn(echo, HIGH);
+  distance = (distance * .0343)/2; //duration times the speed of light cm/us
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  //With this we have a working ultrasonic sensor.
+  //Now we need to code for detection of the soda can, that is, set the unit distance our arm
+  //stop moving once a distance is detected. This lower limit is measured from the base of the
+  //hand to our sensor, once this is reached the movement stops and the hand closes.
+  if (distance <= 50 ){
+    Braccio.ServoMovement(20,         Rotation, 45 - 1, 180 - 1, 180 - 1, 90, 73);
+    //Not too sure how to make this if loop function properly, needs to lower by 1 everytime it 
+    //checks distance
+    
+  }
 
-
-void loop() {
   initial();
-
 }
