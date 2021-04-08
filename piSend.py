cc3e4314d1342f74f458d5d75da11eb268d35cc4
terribadStudent <58722304+terribadStudent@users.noreply.github.com>
2021-04-07 21:09:48 -0700

    Update piSend.py

--- piSend.py
+++ piSend.py
@@ -5,24 +5,21 @@
 import serial
 import time
 
+x = 20
+y = -30
+
 if __name__ == '__main__':
-  ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1) #Initializes port
+  ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1) #Initializes port. Increase timeout value if not reading fully.
   #Serial(port, baudrate, bytesize, parity, stopbits, timeout, xonxoff, rtscts, write_timeout, dsrdtr, inter_byte_timeout, exclusive)
   #The port is immediately opened on object creation, when a port is given. It is not opened when port is None and a successive call to open() is required.
   #port is a device name: depending on operating system. e.g. /dev/ttyUSB0 on GNU/Linux or COM3 on Windows.
   ser.flush() #Wait until all data is written.
   
-  #Put float numbers x and y into parentheses of .write()
-  ser.write(11) #Write the bytes to the port. This should be of type bytes.
-  x = ser.readline()
-  ser.write(-22) #Write the bytes to the port. This should be of type bytes.
-  y = ser.readline()
-  print("x = ", x, "\n")
-  print("y = ", y, "\n")
+  ser.write(str(x)"\n".encode('ascii')) #Write the bytes to the port. This should be of type bytes.
+  ser.write(str(y)"\n)".encode('ascii')) #Write the bytes to the port. This should be of type bytes.
   
-  ser.write(-66) #Write the bytes to the port. This should be of type bytes.
-  ser.write(77) #Write the bytes to the port. This should be of type bytes.
-  x = ser.readline()
-  y = ser.readline()
-  print("x = ", x, "\n")
-  print("y = ", y, "\n")
+  #Print to see if values are correct
+  value2 = ser.read_until() #
+  value1 = ser.read_until()
+  print("x = ", value1, "\n")
+  print("y = ", value2, "\n")
