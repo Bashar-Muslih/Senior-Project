8b214136bcdf2d1aa2f9289e101f75b499207ad3
terribadStudent <58722304+terribadStudent@users.noreply.github.com>
2021-04-07 18:24:25 -0700

    Update arduinoRecieve.ino

--- arduinoRecieve.ino
+++ arduinoRecieve.ino
@@ -15,11 +15,11 @@ void setup() {
 }
 void loop() {
     float coord[1]; //Array with 2 values. [x, y]
-        if (Serial.available() > 0) { //Check if the Arduino has received data. This will give you the number of bytes already arrived and stored in the receive buffer.
-            for(int i = 0; i < 2; i++){ //Two iterations: one for 'x', one for 'y'
-                String data = Serial.readStringUntil('\n'); //Define 'data' as the string read.
-                Serial.println(data); //Print 'data'
-                coord[i] = data.toFloat(); //Convert 'data' to a float
-            }
+    if (Serial.available() > 0) { //Check if the Arduino has received data. This will give you the number of bytes already arrived and stored in the receive buffer.
+        for(int i = 0; i < 2; i++){ //Two iterations: one for 'x', one for 'y'
+            String data = Serial.readStringUntil('\n'); //Define 'data' as the string read.
+            Serial.println(data); //Print 'data'
+            coord[i] = data.toFloat(); //Convert 'data' to a float
         }
     }
+}
