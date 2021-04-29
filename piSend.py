f0d21009410ce989159fafba018dc02a97df4bba
terribadStudent <58722304+terribadStudent@users.noreply.github.com>
2021-04-28 19:44:58 -0700

    Update piSend.py

--- piSend.py
+++ piSend.py
@@ -1,21 +1,30 @@
 #I don't know Python.
 #https://pyserial.readthedocs.io/en/latest/pyserial_api.html#classes
+#Code for testing/debugging is commented
 
 #!/usr/bin/env python3
 import serial
 import time
 
-x = 20
-y = -30
+x = +20000
+y = -300000
 
 if __name__ == '__main__':
-  ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1) #Initializes port. Increase timeout value if not reading fully.
-  #Serial(port, baudrate, bytesize, parity, stopbits, timeout, xonxoff, rtscts, write_timeout, dsrdtr, inter_byte_timeout, exclusive)
-  #The port is immediately opened on object creation, when a port is given. It is not opened when port is None and a successive call to open() is required.
-  #port is a device name: depending on operating system. e.g. /dev/ttyUSB0 on GNU/Linux or COM3 on Windows.
-  ser.flush() #Wait until all data is written.
-  
-  time.sleep(1) #Gives Arduino some time.
-  ser.write(str(x).encode('ascii')) #Write the bytes to the port. This should be of type bytes.
-  time.sleep(1) #Wait 1 sec before sending the next bytes
-  ser.write(str(y).encode('ascii')) #Write the bytes to the port. This should be of type bytes.
+    ser = serial.Serial('/dev/ttyACM0', 9600, timeout = 1)
+    ser.flush()
+    time.sleep(1)
+
+    ser.write("\n".encode('ascii'))
+    time.sleep(1)
+    ser.write(str(x).encode('ascii') + b"\n")
+    #print("Sent X = " + str(x))
+    time.sleep(1)   
+    ser.write(str(y).encode('ascii'))
+    #print("Sent Y = " + str(y)) 
+    time.sleep(1)
+
+    #while True:
+        #if ser.in_waiting > 0:
+            #line = ser.readline().decode('ascii').rstrip()
+            #print(line)
+            #time.sleep(1)
