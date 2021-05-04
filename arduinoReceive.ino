d11376521b0c93c0157e2b339e3907b758439cb6
terribadStudent <58722304+terribadStudent@users.noreply.github.com>
2021-05-03 22:33:04 -0700

    Update arduinoReceive.ino

--- arduinoReceive.ino
+++ arduinoReceive.ino
@@ -13,24 +13,31 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  float coord[2]; //Array with 3 values: [x, y] including null at the beginning.
+  float coord[2]; //Array with 2 values. [x, y]
   float X, Y;
   int i = 0;
-  
-  if (Serial.available() > 2) { //Check if the Arduino has received data. This will give you the number of bytes already arrived and stored in the receive buffer.
+  bool coordTransfer = false;
+
+  while(coordTransfer == false){
+    if (Serial.available() > 2) { //Check if the Arduino has received data. This will give you the number of bytes already arrived and stored in the receive buffer.
       Serial.flush();
       while(Serial.available()){
         delay(1000);
         coord[i] = Serial.parseFloat();
         i++;
       }
-      delay(1000);
-      X = coord[0];
-      Y = coord[1];
-      //Serial.print("You sent me X = ");
-      //Serial.println(X);
-      //delay(1000);
-      //Serial.print("You sent me Y = ");
-      //Serial.println(Y);
-      //delay(1000);
+    }
+    if (i == 3){
+      coordTransfer == true;
+    }
   }
+  delay(1000);
+  X = coord[0];
+  Y = coord[1];
+  Serial.print("You sent me X = ");
+  Serial.println(X);
+  delay(1000);
+  Serial.print("You sent me Y = ");
+  Serial.println(Y);
+  delay(1000);
+}
