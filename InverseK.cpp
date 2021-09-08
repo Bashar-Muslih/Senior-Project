2b523bbddba2a51ac4e0bb4d785b5237eba69d69
Bashar <78171470+Bashar-Muslih@users.noreply.github.com>
2021-09-08 14:49:41 -0700

    Inverse Kinematics for Jose

--- InverseK.cpp
+++ InverseK.cpp
@@ -98,7 +98,7 @@ bool _Inverse::_solve(float x, float y, float phi, float& shoulder, float& elbow
 	float beta;
 	if(!_cosrule(_L2.getLength(), R, _L1.getLength(), beta)) return false;
 	
-	// Calculate the inner angle of the elbow
+	// Calcula the inner angle of the elbow
 	float gamma;
 	if(!_cosrule(R, _L1.getLength(), _L2.getLength(), gamma)) return false;
 	
