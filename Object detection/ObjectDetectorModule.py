4408d258c6658ec0c6afeaa3c5685a5a936e4181
jimbrey300 <80287311+jimbrey300@users.noreply.github.com>
2021-10-03 10:50:14 -0700

    Add files via upload

---
+++ Object detection/ObjectDetectorModule.py
@@ -0,0 +1,48 @@
+import cv2
+
+thres = 0.45 # Threshold to detect object
+
+classNames= []
+classFile = '/home/pi/Desktop/Object detection/coco.names'
+with open(classFile,'rt') as f:
+    classNames = f.read().rstrip('\n').split('\n')
+
+configPath = '/home/pi/Desktop/Object detection/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+weightsPath = '/home/pi/Desktop/Object detection/frozen_inference_graph.pb'
+
+net = cv2.dnn_DetectionModel(weightsPath,configPath)
+net.setInputSize(320,320)
+net.setInputScale(1.0/ 127.5)
+net.setInputMean((127.5, 127.5, 127.5))
+net.setInputSwapRB(True)
+
+def getObjects(img,thres,nms,draw = True,objects=[]):
+    classIds, confs, bbox = net.detect(img,confThreshold=thres,nmsThreshold=nms) #to avoid duplication of detection
+    #print(classIds,bbox)
+    if len(objects) ==0: objects = classNames #allows to choose which object to detect and display
+    objectInfo = []
+    if len(classIds) != 0:
+        for classId, confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
+            className = classNames[classId-1]
+            if className in objects: # if the classname is in the object then permorm this
+                objectInfo.append([box, className])
+                if (draw):
+                    cv2.rectangle(img,box,color=(0,255,0),thickness=2)
+                    cv2.putText(img,className.upper(),(box[0]+10,box[1]+30),
+                            cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+                    cv2.putText(img,str(round(confidence*100,2)),(box[0]+200,box[1]+30),
+                            cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+
+    return img,objectInfo
+
+if __name__ == "__main__":
+    cap = cv2.VideoCapture(-1)
+    cap.set(3,640)
+    cap.set(4,480)
+    #cap.set(10,70)
+    while True:
+        success,img = cap.read()
+        result,objectInfo = getObjects(img, 0.45, 0.2, objects=['cup', 'bottle']) #chooses which one to detect
+        print(objectInfo)
+        cv2.imshow("Output",img)
+        cv2.waitKey(1)
